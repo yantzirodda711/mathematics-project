cc2803141671c92909c6693a2f69a55d46ea70cf
yantzirodda711 <yantzirodda711@gmail.com>
2025-03-19 21:05:39 +0000

    Add 13.py

---
+++ src/13.py
@@ -0,0 +1,13 @@
+import numpy as np
+def get_prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
