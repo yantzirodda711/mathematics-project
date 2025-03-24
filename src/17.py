d99a14a034069bfa566d9fd5173dc4e130ece2bc
yantzirodda711 <yantzirodda711@gmail.com>
2025-03-24 09:04:54 +0000

    Add 17.py

---
+++ src/17.py
@@ -0,0 +1,7 @@
+def multiply_numbers(num1: float, num2: float) -> float:
+    result = num1 * num2
+    return result
+
+def add_numbers(num1: float, num2: float) -> float:
+    result = num1 + num2
+    return result
