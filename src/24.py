27be55a5fbc70ba27f80de7033ec562426fbf090
yantzirodda711 <yantzirodda711@gmail.com>
2025-04-05 05:22:36 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Example usage
+num = 37
+print(is_prime(num))
