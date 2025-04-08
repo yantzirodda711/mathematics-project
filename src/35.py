573a93f8d05309e7048ffe219ce543a88520c695
yantzirodda711 <yantzirodda711@gmail.com>
2025-04-08 13:59:21 +0000

    Add 35.py

---
+++ src/35.py
@@ -0,0 +1,15 @@
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(7))
