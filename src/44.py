f776f94a5444a7c196c4cd22088db1d7f196ac18
yantzirodda711 <yantzirodda711@gmail.com>
2025-05-18 22:37:03 +0000

    Add 44.py

---
+++ src/44.py
@@ -0,0 +1,7 @@
+def calculate_area(radius):
+    area = 3.1415 * radius ** 2
+    return area
+
+radius = 7
+area = calculate_area(radius)
+print(f"The area of a circle with radius {radius} is: {area}")
