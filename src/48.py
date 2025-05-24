b12b2085a9ef416de02bee1e12479d8c8e41fdd0
yantzirodda711 <yantzirodda711@gmail.com>
2025-05-24 19:23:01 +0000

    Add 48.py

---
+++ src/48.py
@@ -0,0 +1,13 @@
+def calculate_cube_volume(side_length):
+    """
+    Calculate the volume of a cube given its side length.
+    
+    Parameters:
+    side_length (float): The length of one side of the cube in feet.
+    
+    Returns:
+    float: The volume of the cube in cubic feet.
+    """
+    return side_length ** 3
+
+calculate_cube_volume(5)
